dd2ba4494ee8dca731aa170e05b87f9ea37b09e2
erinilis <389897773@qq.com>
2023-07-28 16:36:02 +0800

    更新插件

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ __version__ = "0.0.11"
 
 if __name__ == "__main__":
     logger.info("EX-M v%s" % __version__)
+    logger.info("浏览器插件: [https://greasyfork.org/scripts/471903-ex-m/code/EX-M.user.js]")
     initConfig()
     run_server()
     os.system("pause")
